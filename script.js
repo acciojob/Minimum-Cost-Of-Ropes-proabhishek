b78220c7c74b5194234df5afb7dd2e66370109c6
Abhishek <28715657+proabhishek@users.noreply.github.com>
2023-02-14 18:14:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,20 @@
-function findMinCost() {
-	const arr = document.querySelector('input')[0].value.split('');
-	alert(arr);
+function findMinCost(e) {
+	//alert(e);
+	e.preventDefault();
+	const arr = document.querySelector('input').value.split(',');
+	//arr.sort(function(a,b) {return a-b});
+	var cost = 0;
+	//alert(cost);
+	while(arr.length > 1) {
+		var first = arr.shift();
+		var second = arr.shift();
+		var res = Number(first) + Number(second);
+		arr.push(res);
+		cost += res;
+		arr.sort(function (a,b) { return a-b; });
+	}
+    //alert(cost);
+	document.querySelector('#result').textContent = cost;
 }
+
+document.getELe
