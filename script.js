05086536350e3921f7d2b223ff12b529536b54a4
Abhishek <28715657+proabhishek@users.noreply.github.com>
2023-02-14 20:50:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-function minCostToFormRope(event) {
+function minCostToFormRope(a) {
 	
-	event.preventDefault();
+	a.preventDefault();
 	var inputElement = document.querySelector('input').value;
 	var arr = inputElement.split(',');
 	arr.sort(function (a,b) { return a-b});
@@ -13,10 +13,10 @@ function minCostToFormRope(event) {
     var cost = 0;
 	while(arr.length > 1) {
 		var res = Number(arr[0]) + Number(arr[1]);
+		arr.splice(0,2);
+		
 		arr.push(res);
 		cost += res;
-        arr.shift();
-		arr.shift();
 
 		arr.sort(function (a,b) { return a-b});
 	}
